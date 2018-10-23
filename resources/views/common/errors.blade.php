dacaa220903ee4dba6f3d4f972b8b3f64bdf3c1c
3A417012 <nds10745@gmail.com>
2018-10-23 14:18:10 +0800

    Adding error messenge

--- resources/views/common/errors.blade.php
+++ resources/views/common/errors.blade.php
@@ -13,3 +13,15 @@ Route::post('/task', function (Request $request) {
     // 建立該任務...
 });
 ?>
+@if (count($errors) > 0)
+    <!-- 表單錯誤清單 -->
+    <div class="alert alert-danger">
+        <strong>哎呀！出了些問題！</strong>
+        <br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+	@endif
